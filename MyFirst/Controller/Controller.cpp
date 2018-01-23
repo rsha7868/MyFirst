02e6cf2699583caf8679f892e001d57746d3882d
Sharp <rsha7868@740s-pl-127.local>
2018-01-23 12:46:17 -0700

    Made a controller as a method.

---
+++ MyFirst/Controller/Controller.cpp
@@ -0,0 +1,9 @@
+//
+//  Controller.cpp
+//  MyFirst
+//
+//  Created by Sharp, Ryan on 1/23/18.
+//  Copyright © 2018 Sharp, Ryan. All rights reserved.
+//
+
+#include "Controller.hpp"
---
+++ MyFirst/Controller/Controller.hpp
@@ -0,0 +1,21 @@
+//
+//  Controller.hpp
+//  MyFirst
+//
+//  Created by Sharp, Ryan on 1/23/18.
+//  Copyright © 2018 Sharp, Ryan. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <iostream>
+
+class Controller
+{
+public:
+    Controller();
+    void start();
+};
+
+#endif /* Controller_hpp */
